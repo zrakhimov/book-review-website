473dbe996f54ee42d0298b48e6fddc0ebe08c995
Rakhimov <zokir.rakhimov@gmail.com>
2019-03-15 15:46:09 -0400

    Register logic implemented

--- application.py
+++ application.py
@@ -1,9 +1,11 @@
 import os
 
-from flask import Flask, session, render_template, request
+from flask import Flask, session, render_template, redirect, request
 from flask_session import Session
 from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker
+
+from werkzeug.security import check_password_hash, generate_password_hash
 
 app = Flask(__name__)
 
@@ -29,11 +31,14 @@ def index():
 
 @app.route("/register", methods=["GET", "POST"])
 def register():
+
     # if GET, show the registration form
     if request.method == "GET":
         return render_template("register.html")
+
     # if POST, validate and commit to database
-    elif request.method == "POST":
+
+    if request.method == "POST":
         #if form values are empty show error
         if not request.form.get("first_name"):
             return render_template("error.html", message="Must provide First Name")
@@ -45,7 +50,24 @@ def register():
             return render_template("error.html", message="Must provide password")
         elif request.form.get("password1") != request.form.get("password2"):
             return render_template("error.html", message="Password does not match")
-        return render_template("error.html", message="Success")
+        ## end validation
+        else :
+            ## assign to variables
+            first_name = request.form.get("first_name")
+            last_name = request.form.get("last_name")
+            email = request.form.get("email")
+            password = request.form.get("password1")
+            # try to commit to database, raise error if any
+            try:
+                db.execute("INSERT INTO users (firstname, lastname, email, password) VALUES (:firstname, :lastname, :email, :password)",
+                               {"firstname": email, "lastname": last_name, "email":email, "password": generate_password_hash(password)})
+            except Exception as e:
+                return render_template("error.html", message=e)
+
+            db.commit()
+
+            #success - redirect to login
+            return redirect("/login")
 
 
 @app.route("/login", methods=["GET", "POST"])
